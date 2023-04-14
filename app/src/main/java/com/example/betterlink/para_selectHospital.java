fa7f447459d6000f6452caed5a2363ba1a41fbc7
Tiya Mehta <tiyakev@gmail.com>
2023-04-14 14:41:10 +0530

    second commit

---
+++ app/src/main/java/com/example/betterlink/para_selectHospital.java
@@ -0,0 +1,14 @@
+package com.example.betterlink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class para_selectHospital extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_para_select_hospital);
+    }
+}
