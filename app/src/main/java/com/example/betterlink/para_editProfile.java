f87d880beaae7d80c4e6da7c195689b65bc94eae
Tiya Mehta <tiyakev@gmail.com>
2023-04-16 14:41:30 +0530

    second commit

---
+++ app/src/main/java/com/example/betterlink/para_editProfile.java
@@ -0,0 +1,14 @@
+package com.example.betterlink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class para_editProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_para_edit_profile);
+    }
+}
