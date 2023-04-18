fc4e582ba94640ba5ad313b7e7cb315c773ba533
Tiya Mehta <tiyakev@gmail.com>
2023-04-18 14:41:53 +0530

    second commit

---
+++ app/src/main/java/com/example/betterlink/para_addForm.java
@@ -0,0 +1,16 @@
+package com.example.betterlink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class para_addForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_para_add_form);
+
+    }
+}
